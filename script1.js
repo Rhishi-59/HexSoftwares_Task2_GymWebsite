5aaf0b5552e047d3e4d3f641cbe387aad4bdec10
Rhishi-59 <nairrhishi5@gmail.com>
2024-12-27 09:48:56 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Physical Fitness', 'Spiritual Strength', 'Mental Strength', 'Strength Training','Fat Loss','Weight Gain'],
+    typeSpeed: 60,
+  });
